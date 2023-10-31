c8566fe245df5cf6c50b235987deae18ab32c898
Jefferson <jefferson.senac1@yahoo.com>
2023-10-31 14:34:05 -0300

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,42 +1,46 @@
-import './index.css'
-import { BiMenuAltRight } from 'react-icons/bi'
-import {UseState, useState} from 'react'
-import OutsideClickHandler from 'react-outside-click-handler'
+import './index.css';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
-export default function Header(){
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const [MenuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-const getMenuStyles = (MenuOpen) => {
-    if(document.documentElement.clientWidth <= 800){
-        return{ right: !MenuOpen && '-100%'}
-    }
-}
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
-    return (
-        <section className='headerContainer'>
-         <div className='h-container'>
-            <div>
-                 <img src="./logo.png" alt="logo" width={100}/>
-            </div>
-            <div className='h-menu'>
-                <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
-                <nav style={getMenuStyles(MenuOpen)}>
-                    <a href="">Residencies</a>
-                    <a href="">Residencies</a>
-                    <a href="">Residencies</a>
-                    <a href="">Residencies</a>
-                    <button className='button'>
-                        <a href="">Contact</a>
-                    </button>
-                </nav>
-                </OutsideClickHandler>
-            </div>
-        </div> 
-        <div className='mobile-icon' onClick={() => setMenuOpen((prev)=>!prev)}>
-            <BiMenuAltRight size={30}/>
+  const menuStyles = {
+    right: menuOpen ? 0 : '-100%',
+  };
+
+  return (
+    <section className='headerContainer'>
+      <div className='h-container'>
+        <div>
+          <img src="./logo.png" alt="logo" width={100} />
         </div>
-        </section>
-    )
+        <div className='h-menu'>
+          <OutsideClickHandler onOutsideClick={closeMenu}>
+            <nav style={menuStyles}>
+              <a href="">Residencies</a>
+              <a href="">Residencies</a>
+              <a href="">Residencies</a>
+              <a href="">Residencies</a>
+              <button className='button'>
+                <a href="">Contact</a>
+              </button>
+            </nav>
+          </OutsideClickHandler>
+        </div>
+      </div>
+      <div className='mobile-icon' onClick={toggleMenu}>
+        <BiMenuAltRight size={30} />
+      </div>
+    </section>
+  );
 }
-
